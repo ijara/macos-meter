800279b8f6877a444b7c3f5e8ec98440d778abce
Ignacio Jara <ijara756@gmail.com>
2019-07-14 22:51:03 -0400

    Initial Commit

---
+++ macos-meter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  macos-meter
+//
+//  Created by Ignacio Jara on 14-07-19.
+//  Copyright © 2019 Ignacio Jara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
