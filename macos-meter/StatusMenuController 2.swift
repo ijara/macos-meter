1e4caacd66d367cb95fa87ee6afdaf56b213885f
Ignacio Jara <ijara756@gmail.com>
2019-07-24 22:30:28 -0400

    actualizaciones varias

---
+++ macos-meter/StatusMenuController 2.swift
@@ -0,0 +1,70 @@
+//
+//  StatusMenuController.swift
+//  macos-meter
+//
+//  Created by Ignacio Jara on 14-07-19.
+//  Copyright © 2019 Ignacio Jara. All rights reserved.
+//
+
+import Cocoa
+
+class StatusMenuController: NSObject , PreferencesWindowDelegate , WeatherAPIDelegate {
+
+    //outlets
+    @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var weatherView: WeatherView!
+   
+    //var
+    var weatherMenuItem: NSMenuItem!
+    var weatherAPI: WeatherAPI!
+    var preferencesWindow: PreferencesWindow!
+    //let
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    
+    @IBAction func preferencesClicked(_ sender: NSMenuItem) {
+        preferencesWindow.showWindow(nil)
+    }
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        exit(0)
+    }
+    
+    
+    
+    @IBAction func updateClicked(_ sender: NSMenuItem) {
+        updateWeather()
+    }
+
+    override func awakeFromNib() {
+        let icon = NSImage(named: "statusIcon")
+        icon?.isTemplate = true // best for dark mode
+        //statusItem.image = icon
+        statusItem.title = "loading"
+        statusItem.menu = statusMenu
+        weatherAPI = WeatherAPI(delegate: self)
+        weatherMenuItem = statusMenu.item(withTitle: "weather")
+        weatherMenuItem.view = weatherView
+        preferencesWindow = PreferencesWindow()
+        preferencesWindow.delegate = self as PreferencesWindowDelegate
+        updateWeather()
+       
+    }
+    func preferencesDidUpdate() {
+        updateWeather()
+    }
+    func weatherDidUpdate(_ weather: Weather) {
+        NSLog(weather.description)
+    }
+    func updateWeather() {
+        let DEFAULT_CITY = "Santiago,CL"
+
+        let defaults = UserDefaults.standard
+        let city = defaults.string(forKey: "city") ?? DEFAULT_CITY
+        weatherAPI.fetchWeather(city) {
+            weather in self.weatherView.update(weather)
+            self.statusItem.title = weather.description
+        }
+    }
+   
+}
